c53001a120942c72c41f2c496360369a1123af77
Patrick <psterz98@gmail.com>
2022-07-27 19:13:27 +0200

    initialisation of objects

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -1,7 +1,9 @@
 class MoveableObject {
     x = 120;
-    y = 400;
+    y = 250;
     img;
+    height= 150;
+    width= 100;
 
     loadImage(path) {
         this.img = new Image(); //this.img = document.getElementById('image') <img id="image"
