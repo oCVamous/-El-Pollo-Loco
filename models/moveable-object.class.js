ec618ec7e3410ea047f1b9a69d2ae5e0112d3ad0
Patrick <psterz98@gmail.com>
2022-07-28 22:44:34 +0200

    imageCache

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -4,13 +4,29 @@ class MoveableObject {
     img;
     height= 150;
     width= 100;
+    imageCache = {};
 
+    //loadImage('img/test.png);
     loadImage(path) {
         this.img = new Image(); //this.img = document.getElementById('image') <img id="image"
         this.img.src = path;
     }
+
     moveRight() {
         console.log('Moving-right');
+    }
+
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ... ]
+     */
+    loadImages(arr) {
+            arr.forEach((path) => {
+                let img = new Image();
+            img.src = path;
+            this.imageCache[path] = path;
+            })
+        
     }
 
     moveLeft() {
