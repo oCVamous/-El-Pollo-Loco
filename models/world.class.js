88fe6980a036fa05b15ef183d136ac0d6e58c8f1
Patrick <psterz98@gmail.com>
2022-07-28 20:34:48 +0200

    bug forEach

--- models/world.class.js
+++ models/world.class.js
@@ -9,6 +9,9 @@ class World {
     clouds = [
         new Cloud(),
     ];
+    backgoundObject = [
+        new BackgroundObject("img/5_background/complete_background.png")
+    ];
     canvas;
     ctx;
 
@@ -21,19 +24,27 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-        })
-
-        this.clouds.forEach(cloud => {
-            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-        })
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.BackgroundObjects);
 
         // Draw() wird immer wieder aufgerufen
         self = this;
         requestAnimationFrame(function() {
             self.draw();
         });
+
+        
     }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+    };
 }
