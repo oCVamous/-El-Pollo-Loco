2dfc94bf95466395941a6918dbaf2ee06da7fec0
Patrick <psterz98@gmail.com>
2022-08-01 10:53:22 +0200

    move camera

--- models/world.class.js
+++ models/world.class.js
@@ -19,6 +19,7 @@ class World {
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -35,10 +36,14 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.ctx.translate(this.camera_x, 0);
+
         this.addObjectsToMap(this.BackgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.clouds);
         this.addObjectsToMap(this.enemies);
+
+        this.ctx.translate(-this.camera_x, 0);
         
 
         // Draw() wird immer wieder aufgerufen
