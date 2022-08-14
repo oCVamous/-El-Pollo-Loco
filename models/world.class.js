150741fbead8b59f418c370d9a2746a1d7d74a3a
Patrick <psterz98@gmail.com>
2022-08-14 23:59:07 +0200

    added bottle

--- models/world.class.js
+++ models/world.class.js
@@ -6,6 +6,7 @@ class World {
     canvas;
     keyboard;
     camera_x = -100;
+    throwableObjects = [];
 
     statusBarHealth = new StatusBarHealth();
     statusBarBottle = new StatusBarBottle();
@@ -18,22 +19,35 @@ class World {
         this.keyboard = keyboard;
         this.draw();
         this.setWorld();
-        this.checkCollisions();
+        this.run();
     }
-
+    
     setWorld() {
         this.character.world = this;
     }
 
-    checkCollisions() {
+    run() {
         setInterval(() => {
-            this.level.enemies.forEach((enemy) => {
-                if(this.character.isColliding(enemy)) {
-                    this.character.hit();
-                    this.statusBarHealth.setPercentage(this.character.energy);
-                }
-            });
+
+            this.checkCollisions();
+            this.checkThrowObjects();
         }, 200);
+    }
+
+    checkCollisions() {
+        this.level.enemies.forEach((enemy) => {
+            if(this.character.isColliding(enemy)) {
+                this.character.hit();
+                this.statusBarHealth.setPercentage(this.character.energy);
+            }
+        });
+    }
+
+    checkThrowObjects() {
+        if(this.keyboard.D) {
+            let bottle = new ThrowableObject(this.character.x +100, this.character.y +100);
+            this.throwableObjects.push(bottle);
+        }
     }
 
     draw() {
@@ -56,6 +70,7 @@ class World {
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies);
+        this.addObjectsToMap(this.throwableObjects);
 
         this.ctx.translate(-this.camera_x, 0);
 
